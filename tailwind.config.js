a7bf981b511303af06cb4728ea5aed44a1fa7e6f
Mohammed785 <mohammednady128@gmail.com>
2024-03-26 18:11:02 +0200

    create post templs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'text': {
+        text: {
           50: 'var(--text-50)',
           100: 'var(--text-100)',
           200: 'var(--text-200)',
@@ -20,7 +20,7 @@ module.exports = {
           900: 'var(--text-900)',
           950: 'var(--text-950)',
         },
-        'background': {
+        background: {
           50: 'var(--background-50)',
           100: 'var(--background-100)',
           200: 'var(--background-200)',
@@ -33,7 +33,7 @@ module.exports = {
           900: 'var(--background-900)',
           950: 'var(--background-950)',
         },
-        'primary': {
+        primary: {
           50: 'var(--primary-50)',
           100: 'var(--primary-100)',
           200: 'var(--primary-200)',
@@ -46,7 +46,7 @@ module.exports = {
           900: 'var(--primary-900)',
           950: 'var(--primary-950)',
         },
-        'secondary': {
+        secondary: {
           50: 'var(--secondary-50)',
           100: 'var(--secondary-100)',
           200: 'var(--secondary-200)',
@@ -59,7 +59,7 @@ module.exports = {
           900: 'var(--secondary-900)',
           950: 'var(--secondary-950)',
         },
-        'accent': {
+        accent: {
           50: 'var(--accent-50)',
           100: 'var(--accent-100)',
           200: 'var(--accent-200)',
